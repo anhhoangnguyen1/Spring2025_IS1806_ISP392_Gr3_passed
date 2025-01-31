f4401ea31257d864033f3749d3d744e3f9d493ec
MaiPN <phanngocmai2411@gmail.com>
2025-02-01 02:19:49 +0700

    update user login

--- ISP392_Project/src/java/dal/AccountDAO.java
+++ ISP392_Project/src/java/dal/AccountDAO.java
@@ -5,15 +5,58 @@
 package dal;
 
 import Model.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author ADMIN
  */
-public class AccountDAO implements I_DAO<Users>{
+public class AccountDAO implements I_DAO<Users> {
+
+    public static AccountDAO INSTANCE = new AccountDAO();
+    private static final Logger LOGGER = Logger.getLogger(AccountDAO.class.getName());
+    private DBContext dbContext;
+    private Connection connection;
+
+    public AccountDAO() {
+        dbContext = new DBContext();
+        connection = dbContext.connection;  // Lấy kết nối từ DBContext
+    }
+
+    // authenticate the login avaiable in the database
+    public Users authenticate(String username, String password) {
+        String sql = "SELECT Users.user_id, Users.username, Users.password, Role.name "
+                + "FROM Users "
+                + "JOIN Role ON Users.role_id = Role.role_id "
+                + "WHERE Users.username = ? AND Users.password = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, username);
+            ps.setString(2, password);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    Users login = new Users();
+                    login.setUserId(rs.getInt("user_id"));
+                    login.setUsername(rs.getString("username"));
+                    login.setPassword(rs.getString("password"));
+
+                    Role role = new Role();
+                    role.setName(rs.getString("name"));
+                    login.setRole(role);
+
+                    return login;
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Error during authentication: {0}", e.getMessage());
+        }
+        return null;
+    }
 
     @Override
     public List<Users> findAll() {
@@ -44,5 +87,15 @@ public class AccountDAO implements I_DAO<Users>{
     public Users getFromResultSet(ResultSet resultSet) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
+    public static void main(String[] args) {
+        AccountDAO dao = new AccountDAO();
+        
+        Users user1 = dao.authenticate("admin", "123456");
+        if (user1 != null) {
+            System.out.println("Đăng nhập thành công: " + user1.getUsername() + " - Role: " + user1.getRole().getName());
+        } else {
+            System.out.println("Đăng nhập thất bại (admin, 123456)");
+        }
+    }
 }
