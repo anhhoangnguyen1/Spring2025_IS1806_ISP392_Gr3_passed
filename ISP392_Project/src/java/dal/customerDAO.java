07429700d716af98764300425c8555704834e2f0
sa <danhpthe180490@fpt.edu.vn>
2025-02-12 10:05:28 +0700

    Fix CustomerDAO

--- ISP392_Project/src/java/dal/customerDAO.java
+++ ISP392_Project/src/java/dal/customerDAO.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DAO;
+package dal;
 
 /**
  *
  * @author phamh
  */
-public class customerDAO extends DBcontext{
+public class customerDAO extends DBContext{
     
 }
