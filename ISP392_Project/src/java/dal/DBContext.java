9b1d959e2afd94ca58fcc612e3b2163238e653fe
sa <danhpthe180490@fpt.edu.vn>
2025-02-16 21:12:53 +0700

    Fixing connection to database

--- ISP392_Project/src/java/dal/DBContext.java
+++ ISP392_Project/src/java/dal/DBContext.java
@@ -5,43 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class DBContext {
     protected Connection connection;
-    protected PreparedStatement statement;
-    protected ResultSet resultSet;
 
     public DBContext() {
         try {
             String username = "root";
             String password = "123456";
-            String url = "jdbc:mysql://localhost:3306/ISP392_Project2";
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/ISP392_Project2"; 
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public void closeResources() {
-        try {
-            if (resultSet != null) {
-                resultSet.close();
-            }
-            if (statement != null) {
-                statement.close();
-            }
-            if (connection != null) {
-                connection.close();
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
     public Connection getConnection() {
-        return new DBContext().connection;
+        return connection;
     }
 }
